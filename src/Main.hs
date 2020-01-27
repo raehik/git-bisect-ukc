6b1795529f7dacbe5044e669c09c2d9fcf84d98f
Ben Orchard <thefirstmuffinman@gmail.com>
2020-01-27 11:26:09 +0000

    WOWEE DONE BOYS

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Data.HashMap.Lazy as HM
+import Data.HashSet as HS
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
@@ -18,16 +19,16 @@ data Message
 type GitCommitName = String
 type GitCommit = (GitCommitName, [GitCommitName])
 type GitGraph = [GitCommit]
-type GitGraphInternal = HashMap GitCommitName ([GitCommitName], Int)
-
-revprepend_list [] y = y
-revprepend_list (x1:xs) y = revprepend_list xs (x1:y)
+type GitGraphInternal = HashMap GitCommitName [GitCommitName]
+data GitCommitStatus
+    = GitCommitBad
+    | GitCommitGood
 
 git_nodelist_to_internal :: GitGraph -> GitGraphInternal
 git_nodelist_to_internal' :: GitGraph -> GitGraphInternal -> GitGraphInternal
 git_nodelist_to_internal list = git_nodelist_to_internal' list HM.empty
 git_nodelist_to_internal' [] graph = graph
-git_nodelist_to_internal' ((commit, parents):list) graph = git_nodelist_to_internal' list (HM.insert commit (parents, 0) graph)
+git_nodelist_to_internal' ((commit, parents):list) graph = git_nodelist_to_internal' list (HM.insert commit parents graph)
 
 graph_01_ex_lin = [("a", []), ("b", ["a"]), ("c", ["b"])]
 graph_02_git_keep = [
@@ -55,29 +56,78 @@ graph_02_git_keep = [
     ("y4", []),
     ("y3", [])
     ]
+graph_03_david_simple = [
+    ("a", []), -- good
+    ("b", ["a"]),
+    ("c", ["a"]),
+    ("d", ["b", "c"]),
+    ("e", ["c"]),
+    ("f", ["d", "e"]) -- bad
+    ]
 
-select_from_hashmap graph wanted_keys = select_from_hashmap' graph wanted_keys HM.empty
-select_from_hashmap' _ [] filtered_graph = filtered_graph
-select_from_hashmap' graph (wanted_key:wanted_keys) filtered_graph =
-    select_from_hashmap' graph wanted_keys (HM.insert wanted_key (graph ! wanted_key) filtered_graph)
-
-filter_graph :: [GitCommitName] -> GitCommitName -> GitGraphInternal -> GitGraphInternal
-filter_graph' :: [GitCommitName] -> GitGraphInternal -> [GitCommitName] -> [GitCommitName] -> GitGraphInternal
-filter_graph c_good c_bad graph =
-    filter_graph' c_good graph [c_bad] (case HM.lookup c_bad graph of Nothing -> []; Just (parents, _) -> parents)
-filter_graph' _ graph c_selected [] =
-    select_from_hashmap graph c_selected
-filter_graph' c_good graph c_selected (c_current:c_workstack)
-    | c_current `elem` c_good =
-        filter_graph' c_good graph c_selected c_workstack
+git_bisect_ro graph c_good c_bad
+    | subgraph_size == 1 = c_bad
     | otherwise =
-        case HM.lookup c_current graph of
-            Nothing -> HM.empty -- TODO malformed graph
-            Just ([], _) -> filter_graph' c_good graph (c_current:c_selected) c_workstack
-            Just (parents, _) -> filter_graph' c_good graph (c_current:c_selected) (revprepend_list parents c_workstack)
+        let commit = select_bisect_commit ancestors subgraph_size Nothing (HM.keys ancestors)
+        in case request_commit_status commit of
+            GitCommitGood   -> git_bisect_ro graph  (HS.insert commit c_good)  c_bad
+            GitCommitBad    -> git_bisect_ro graph  c_good                     commit
+    where
+        ancestors = filter_and_calculate_ancestors graph c_good HM.empty [c_bad]
+        subgraph_size = HS.size (ancestors ! c_bad)
 
---select_bisect_commit :: GitGraphInternal -> Int -> [GitCommitName] -> GitCommitName
---select_bisect_commit graph size stack
+-- TODO
+request_commit_status commit = GitCommitBad
 
--- git_bisect [c_good] c_bad graph
---git_bisect :: [GitCommitName] -> GitCommitName -> GitGraphInternal -> GitCommitName
+-- TODO
+select_bisect_commit ancestors graph_size (Just (commit, commit_value)) [] = commit
+select_bisect_commit ancestors graph_size current_best (commit:commit_stack)
+    | commit_value == (floor ((fromIntegral graph_size)/2)) = commit
+    | otherwise =
+        let new_best_commit = case current_best of
+                Nothing -> Just (commit, commit_value)
+                Just (cur_best_commit, cur_best_value) ->
+                    if   commit_value > cur_best_value
+                    then Just (commit, commit_value)
+                    else Just (cur_best_commit, cur_best_value)
+        in select_bisect_commit ancestors graph_size new_best_commit commit_stack
+    where commit_num_of_ancestors = HS.size (ancestors ! commit)
+          commit_value = min commit_num_of_ancestors (graph_size - commit_num_of_ancestors)
+
+filter_and_calculate_ancestors :: GitGraphInternal -> HashSet GitCommitName -> HashMap GitCommitName (HashSet GitCommitName) -> [GitCommitName] -> HashMap GitCommitName (HashSet GitCommitName)
+filter_and_calculate_ancestors graph    _       ancestors   []
+    = ancestors
+filter_and_calculate_ancestors graph    c_good  ancestors   (commit:stack)
+    = let
+        ancestors_or_schedule = ancestor_folder ancestors c_good (graph ! commit) (Left (HS.singleton commit))
+      in
+        case ancestors_or_schedule of
+            Left calculated_ancestors -> filter_and_calculate_ancestors graph c_good (HM.insert commit calculated_ancestors ancestors) stack
+            Right new_stack     -> filter_and_calculate_ancestors graph c_good ancestors (revprepend_list new_stack (commit:stack))
+
+revprepend_list [] y = y
+revprepend_list (x:xs) y = revprepend_list xs (x:y)
+
+ancestor_folder :: HashMap GitCommitName (HashSet GitCommitName) -> HashSet GitCommitName -> [GitCommitName] -> Either (HashSet GitCommitName) [GitCommitName] -> Either (HashSet GitCommitName) [GitCommitName]
+ancestor_folder ancestors c_good []                 ancestors_or_schedule
+    = ancestors_or_schedule
+ancestor_folder ancestors c_good (commit:commits)   ancestors_or_schedule
+    | commit `elem` c_good = ancestor_folder ancestors c_good commits ancestors_or_schedule
+    | otherwise =
+        let
+            commit_ancestors = HM.lookup commit ancestors
+        in
+            let
+                next_ancestors_or_schedule =
+                    case commit_ancestors of
+                        Nothing ->
+                            case ancestors_or_schedule of
+                                Left _                  -> Right [commit]
+                                Right to_schedule       -> Right (commit:to_schedule)
+                        Just p_a ->
+                            case ancestors_or_schedule of
+                                Left ancestors_so_far   -> Left (HS.union p_a ancestors_so_far)
+                                Right to_schedule       -> Right to_schedule
+            in
+                ancestor_folder ancestors c_good commits next_ancestors_or_schedule
+
