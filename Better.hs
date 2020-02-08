5fc7cbd65cfa5bf089c8220112cbe96561d61b54
Ben Orchard <thefirstmuffinman@gmail.com>
2020-02-08 21:56:34 +0000

    dude..... holy shit

--- Better.hs
+++ Better.hs
@@ -7,6 +7,7 @@ import qualified Data.Map as Map
 import Data.Set (Set)
 import qualified Data.Set as Set
 import Control.Monad (mapM_)
+import qualified Data.Dequeue as Dequeue
 
 git_repo_01 = [
     ("a", []), -- good
@@ -19,19 +20,7 @@ git_repo_01 = [
     ("x", ["g"]) -- this should be filtered
     ]
 
---git_repo_to_internal :: [(String, [String])] -> Map String ([String], [String])
---git_repo_to_internal c_bad l = git_repo_to_internal' (git_repo_subgraph c_bad (Map.fromList l)) Map.empty
---git_repo_to_internal' [] m = m
---git_repo_to_internal' ((c, cps):cs) m =
-    -- for cp in cps, update cp in m
-    -- then update c in m
---    let    map_updated_children = foldl (map_list_merge c) m cps
---    in let map_next = Map.insertWith map_value_concat c (cps, []) map_updated_children
---    in     git_repo_to_internal' cs map_next
-
 git_repo_calculate_children g = Map.foldrWithKey git_repo_map_fold_children g g
-
---git_repo_map_fold_children c (cps, ccs) m = Map.insertWith map_value_concat c (cps, []) (foldl (map_list_merge c) m cps)
 git_repo_map_fold_children c (cps, ccs) m = foldl (map_list_merge c) m cps
 
 map_list_merge c m cp = Map.insertWith map_value_concat cp ([], [c]) m
@@ -60,12 +49,43 @@ git_repo_list_to_map' g ((c, cps):cs) = git_repo_list_to_map' (Map.insert c (cps
 revprepend_list [] y = y
 revprepend_list (x:xs) y = revprepend_list xs (x:y)
 
---git_repo_to_string g = map (++ "\n" . git_commit_to_string) (Map.assocs g)
 git_repo_to_string g = Map.foldrWithKey (\k x ks -> ks ++ git_commit_to_string k x ++ "\n") "" g
---git_repo_print g = mapM_ git_commit_print (Map.assocs g)
 
 git_commit_to_string c (cps, ccs) =
     c
     ++ "\n  P: " ++ show cps
     ++ "\n  C: " ++ show ccs
---git_commit_print (c, (cps, ccs)) = print c
+
+--git_repo_calculate_descendants_and_get_heads :: a -> Map a ([a], [a]) -> (Map a Int, [a])
+git_repo_calculate_descendants_and_get_heads c_bad g =
+    git_repo_calculate_descendants_and_get_heads' g (Map.singleton c_bad 1, []) (Dequeue.pushBack Dequeue.empty c_bad)
+git_repo_calculate_descendants_and_get_heads' :: Ord a => Map a ([a], [a]) -> (Map a Int, [a]) -> Dequeue.BankersDequeue a -> (Map a Int, [a])
+git_repo_calculate_descendants_and_get_heads' g (descs, heads) queue =
+    case Dequeue.popFront queue of
+        Nothing -> (descs, heads)
+        Just (c, queue') ->
+            case Map.lookup c g of
+                -- TODO: this Nothing is an error, should be unreachable
+                Nothing -> git_repo_calculate_descendants_and_get_heads' g (descs, heads) queue'
+                Just ([], _) ->
+                    git_repo_calculate_descendants_and_get_heads' g (descs, (c:heads)) queue'
+                Just (cps, _) ->
+                    case Map.lookup c descs of
+                        -- TODO: again, weird error case
+                        Nothing -> git_repo_calculate_descendants_and_get_heads' g (descs, heads) queue'
+                        Just rank ->
+                            let (descs', queue'') = update_descs_and_queue cps (rank+1) descs queue'
+                            --let    descs'  = update_descs cps rank descs
+                            --in let queue'' = push_back_list_to_queue cps queue'
+                            in  git_repo_calculate_descendants_and_get_heads' g (descs', heads) queue''
+--git_repo_calculate_ancestors :: Map a ([a], [a]) -> (Map a Int, [a])
+push_back_list_to_queue [] q = q
+push_back_list_to_queue (x:xs) q = push_back_list_to_queue xs (Dequeue.pushBack q x)
+update_descs cps rank descs = foldl (\ds c -> Map.insertWith (\new_value old_value -> old_value) c (rank+1) ds) descs cps
+
+--update_descs_and_queue :: Ord a => [a] -> Int -> Map a Int -> Dequeue.BankersDequeue a -> (Map a Int, Dequeue.BankersDequeue a)
+update_descs_and_queue [] rank descs q = (descs, q)
+update_descs_and_queue (cp:cps) rank descs q =
+    case Map.lookup cp descs of
+        Nothing -> update_descs_and_queue cps rank (Map.insert cp rank descs) (Dequeue.pushBack q cp) -- RANK_AND_ADD_TO_QUEUE
+        Just _ -> update_descs_and_queue cps rank descs q
