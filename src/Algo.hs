e984767a501e6056c3b7b80c1d7d94289654324a
Ben Orchard <thefirstmuffinman@gmail.com>
2020-02-28 10:47:34 +0000

    getting real close now

--- src/Algo.hs
+++ src/Algo.hs
@@ -29,17 +29,19 @@ git_commit_show c (c_parents, c_ancs) =
 -- are ones which are in c_bad's ancestry, and not in any of c_good's commits'
 -- ancestry.
 --
--- This is split into two phases:
+-- This is split into three phases:
+--
 --   * create a subgraph starting from c_bad
 --     * for efficiency, we also skip adding any commits in c_good (we'd remove
 --       them later, but this way we don't waste as much time on them)
 --   * remove the subgraphs of each commit in c_good
+--   * edit all commits' ancestors so they don't refer to filtered commits
 --
--- The second phase is less intuitive - please refer to the detailed algorithm
--- discussion in the provided README.
+-- Please refer to the detailed algorithm discussion in the provided README.
 git_repo_subgraph c_good c_bad g =
-    let sg = git_repo_subgraph_from_bad (Set.fromList c_good) g Map.empty [c_bad]
-    in  git_repo_subgraph_remove_goods g sg c_good
+    let    sg  = git_repo_subgraph_from_bad (Set.fromList c_good) g Map.empty [c_bad]
+    in let sg' = git_repo_subgraph_remove_goods g sg c_good
+    in     git_repo_subgraph_filter_ancestors sg'
 
 git_repo_subgraph_from_bad c_good g sg [] = sg
 git_repo_subgraph_from_bad c_good g sg (c:cs) =
@@ -58,6 +60,8 @@ git_repo_subgraph_remove_goods g sg (c:cs) =
         Just (c_parents, c_ancs) ->
             let sg' = foldl (\g c -> Map.delete c g) sg c_parents
             in  git_repo_subgraph_remove_goods g sg' (revprepend_list c_parents cs)
+
+git_repo_subgraph_filter_ancestors g = Map.map (\(c_parents, c_ancs) -> (c_parents, (Set.filter (\c -> Map.member c g) c_ancs))) g
 
 -- Revprepend l2 onto l1.
 revprepend_list l1 l2 = foldl (flip (:)) l1 l2
@@ -79,23 +83,38 @@ git_repo_get_bisect_commit_calc_limit' g (c_best_current, c_best_current_rank) (
     case Map.lookup c_cur g of
         -- we were given a nonexistent commit, nothing to do
         Nothing -> (Nothing, g)
-        Just (c_cur_ps, _) ->
-            case foldl (calculate_ancestors_or_schedule_calculations g) (AncSet (Set.singleton c_cur)) c_cur_ps of
-                -- missing parent ancestors, schedule them before scheduling
-                -- this commit again
-                AncSched c_sched -> git_repo_get_bisect_commit_calc_limit' g (c_best_current, c_best_current_rank) (revprepend_list (c_cur:c_stack) c_sched) remaining_calcs
-                -- successfully calculated ancestor list
-                AncSet c_cur_ancs ->
-                    let    c_cur_rank = min (Set.size c_cur_ancs) ((Map.size g) - (Set.size c_cur_ancs))
-                    in let g' = Map.insert c_cur (c_cur_ps, c_cur_ancs) g
-                    in
-                        if c_cur_rank > c_best_current_rank
-                        then
-                            -- found a better rank: check if it's ideal
-                            if c_cur_rank >= floor ((fromIntegral (Map.size g)) / 2)
-                            then (Just c_cur, g')
-                            else git_repo_get_bisect_commit_calc_limit' g' (c_cur, c_cur_rank) c_stack (remaining_calcs-1)
-                        else git_repo_get_bisect_commit_calc_limit' g' (c_best_current, c_best_current_rank) c_stack (remaining_calcs-1)
+        Just (c_cur_ps, c_cur_ancs) ->
+            if Set.null c_cur_ancs
+            then
+                -- ancestors not yet calculated
+                case foldl (calculate_ancestors_or_schedule_calculations g) (AncSet (Set.singleton c_cur)) c_cur_ps of
+                    -- missing parent ancestors, schedule them before scheduling
+                    -- this commit again
+                    AncSched c_sched -> git_repo_get_bisect_commit_calc_limit' g (c_best_current, c_best_current_rank) (revprepend_list (c_cur:c_stack) c_sched) remaining_calcs
+                    -- successfully calculated ancestor list
+                    AncSet c_cur_ancs' ->
+                        let    c_cur_rank = min (Set.size c_cur_ancs') ((Map.size g) - (Set.size c_cur_ancs'))
+                        in let g' = Map.insert c_cur (c_cur_ps, c_cur_ancs') g
+                        in
+                            if c_cur_rank > c_best_current_rank
+                            then
+                                -- found a better rank: check if it's ideal
+                                if c_cur_rank >= floor ((fromIntegral (Map.size g)) / 2)
+                                then (Just c_cur, g')
+                                else git_repo_get_bisect_commit_calc_limit' g' (c_cur, c_cur_rank) c_stack (remaining_calcs-1)
+                            else git_repo_get_bisect_commit_calc_limit' g' (c_best_current, c_best_current_rank) c_stack (remaining_calcs-1)
+
+            else
+                -- ancestors already calculated
+                let    c_cur_rank = min (Set.size c_cur_ancs) ((Map.size g) - (Set.size c_cur_ancs))
+                in
+                    if c_cur_rank > c_best_current_rank
+                    then
+                        -- found a better rank: check if it's ideal
+                        if c_cur_rank >= floor ((fromIntegral (Map.size g)) / 2)
+                        then (Just c_cur, g)
+                        else git_repo_get_bisect_commit_calc_limit' g (c_cur, c_cur_rank) c_stack remaining_calcs
+                    else git_repo_get_bisect_commit_calc_limit' g (c_best_current, c_best_current_rank) c_stack remaining_calcs
 
 calculate_ancestors_or_schedule_calculations g (AncSet cur_ancs) c =
     case Map.lookup c g of
--- src/Network.hs
+++ src/Network.hs
@@ -10,24 +10,57 @@ import qualified Network.WebSockets as WS
 import Data.ByteString.Lazy (ByteString)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Set (Set)
+import qualified Data.Set as Set
 
---ws_app :: WS.ClientApp ()
-ws_app user_name conn = do
-    T.putStrLn "potentially connected"
-    T.putStrLn "sending user message..."
-    WS.sendTextData conn $ encode (JSONMsgUser user_name)
-    T.putStrLn "receiving problem message..."
-    msgRecv <- WS.receiveData conn :: IO ByteString
-    T.putStrLn "decoding JSON..."
-    case decode msgRecv :: Maybe JSONMsgProblem of
-        Nothing -> T.putStrLn "error decoding JSON"
-        Just pm ->
-            let    pp = problem pm
-            in let g  = git_repo_subgraph [(good pp)] (bad pp) (git_json_repo_list_to_map (dag pp))
+client_init :: String -> WS.ClientApp ()
+client_init user_name conn = do
+    putStrLn "potentially connected"
+    putStrLn "sending user message..."
+    WS.sendTextData conn $ encode (JSONMsgUser (T.pack user_name))
+    putStrLn "receiving problem message..."
+    msg_prob <- WS.receiveData conn :: IO ByteString
+    putStrLn "decoding JSON..."
+    case decode msg_prob :: Maybe JSONMsgProblem of
+        Nothing -> putStrLn "error decoding JSON"
+        Just msg_prob' -> client_solve_problem conn msg_prob'
+
+client_solve_problem conn prob =
+    let pp = problem prob in do
+    c_first_bad <- client_question_loop conn [(good pp)] (bad pp) (git_json_repo_list_to_map (dag pp))
+    case c_first_bad of
+        Nothing -> putStrLn "could not determine first bad commit"
+        Just c_first_bad' -> do
+            WS.sendTextData conn $ encode (JSONMsgSolution c_first_bad')
+            msg_score <- WS.receiveData conn :: IO ByteString
+            case decode msg_score :: Maybe JSONMsgScore of
+                Nothing -> putStrLn "error decoding JSON"
+                Just score -> print score
+
+client_question_loop :: WS.Connection -> [GitCommit] -> GitCommit -> Map GitCommit ([GitCommit], Set GitCommit) -> IO (Maybe GitCommit)
+client_question_loop conn c_good c_bad g =
+    let g' = git_repo_subgraph c_good c_bad g
+    in  if Map.size g' == 1
+        then return (Just c_bad)
+        else
+            let (c_bisect, g'') = git_repo_get_bisect_commit_calc_limit g' c_bad 100000000
             in
-                T.putStrLn "selecting bisect commit..." >>
-                case git_repo_get_bisect_commit_calc_limit g (bad pp) 1 of
-                    Nothing -> T.putStrLn "error finding bisect commit"
-                    Just c -> T.putStrLn c
+                case c_bisect of
+                    Nothing -> return Nothing -- print "error finding bisect commit"
+                    Just c_bisect' -> do
+                        WS.sendTextData conn $ encode (JSONMsgQuestion c_bisect')
+                        msg_answer <- WS.receiveData conn :: IO ByteString
+                        case decode msg_answer :: Maybe JSONMsgAnswer of
+                            Nothing -> return Nothing -- print "error decoding JSON"
+                            Just msg_answer' -> do
+                                let c_status = answer msg_answer'
+                                putStrLn $ "query commit " ++ T.unpack c_bisect' ++ " was " ++ show c_status
+                                let (c_good', c_bad') =
+                                        case c_status of
+                                            GitCommitGood -> ([c_bisect'], c_bad)
+                                            GitCommitBad  -> (c_good, c_bisect')
+                                client_question_loop conn c_good' c_bad' g''
 
-ws_run = WS.runClient "129.12.44.229" 1234 "/" (ws_app "bo207")
+ws_run = WS.runClient "129.12.44.229" 1234 "/" (client_init "bo207")
